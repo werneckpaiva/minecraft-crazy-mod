2ec24fec868bc4f44a71d06bf5f507313ca0cc4e
Ricardo Paiva <werneck.paiva@gmail.com>
2015-12-05 19:12:05 -0200

    Primeira versao do mod da zoeira

---
+++ src/main/java/papai/sinistrao/OvelhaColorida.java
@@ -0,0 +1,37 @@
+package papai.sinistrao;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityCow;
+import net.minecraft.entity.passive.EntitySheep;
+import net.minecraft.event.ClickEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.EntityInteractEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+public class OvelhaColorida {
+
+    @SubscribeEvent
+    public void entityAttachHandler(AttackEntityEvent event){
+        if (!event.target.worldObj.isRemote){
+            Entity entity = event.target;
+            float force = (float) Math.random() * 20;
+            System.out.println("Morreu!!!! "+force);
+            entity.worldObj.createExplosion(event.entityPlayer, entity.posX, entity.posY, entity.posZ, force, true);
+        }
+    }
+
+    @SubscribeEvent
+    public void entityClickHandler(EntityInteractEvent event){
+        if (!event.target.worldObj.isRemote){
+            Entity entity = event.target;
+            if (entity instanceof EntitySheep){
+                EntitySheep sheep = (EntitySheep) entity;
+                sheep.setFleeceColor(sheep.getFleeceColor() + 1);
+            } else if (entity instanceof EntityCow){
+                EntityCow cow = (EntityCow) entity;
+                cow.setFire(5);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/papai/sinistrao/PapaiSinistraoMod.java
@@ -0,0 +1,43 @@
+package papai.sinistrao;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = PapaiSinistraoMod.MODID, version = PapaiSinistraoMod.VERSION, name = PapaiSinistraoMod.NAME)
+public class PapaiSinistraoMod
+{
+    public static final String MODID = "papaisinistraomod";
+    public static final String VERSION = "1.0";
+    public static final String NAME = "Papai Sinistrão";
+
+    @Instance(PapaiSinistraoMod.MODID)
+    public static PapaiSinistraoMod instance; 
+    
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        System.out.println("Papai Sinistrão - Init");
+        MinecraftForge.EVENT_BUS.register(new OvelhaColorida());
+    }
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        System.out.println("Papai Sinistrão - PreInit");
+    }
+    
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        System.out.println("Papai Sinistrão - PostInit");
+    }
+}
+
+
